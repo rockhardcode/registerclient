fe601eca0803ba488bcc6f05764f843d6fe058da
rockhardcode <rakeshrulz99@gmail.com>
2025-02-11 14:31:34 +0530

    fixes

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
 
   const register = async (name, email, password) => {
     try {
-      await axios.post("http://localhost:5000/api/auth/register", {
+      await axios.post("https://registrationserver-1.onrender.com/api/auth/register", {
         name,
         email,
         password,
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://registrationserver-1.onrender.com/api/auth/login", {
         email,
         password,
       });
